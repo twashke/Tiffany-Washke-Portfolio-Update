492c3d401ce2140a1a0a17d7c1e25cbe82688808
Tiffany Washke <twashke@gmail.com>
2021-07-10 13:52:43 -0700

    updated pictures and removed ones not needed

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+// Hamburger Menu function 
+function myFunction() {
+    var x = document.getElementById("links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
